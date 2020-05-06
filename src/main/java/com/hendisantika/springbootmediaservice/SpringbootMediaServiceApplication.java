b493f2825438d5a62bbab10814e1f2774b1a6402
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:28:01 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmediaservice/SpringbootMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMediaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootmediaservice/SpringbootMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMediaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
