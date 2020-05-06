867c3b558e6199f828485e7fc7745f38b8c6d50f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:50:36 +0700

    #16 - Add another Bean in ApiSecurity

--- src/main/java/com/hendisantika/springbootmediaservice/config/ApiSecurity.java
+++ src/main/java/com/hendisantika/springbootmediaservice/config/ApiSecurity.java
@@ -1,8 +1,13 @@
 package com.hendisantika.springbootmediaservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +22,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ApiSecurity extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        authenticationMgr.inMemoryAuthentication().withUser("username")
+                .password("$6y$13$9OzrtK7X4mad/te8m7uhysjfaihjsdfZdJ/fDZFaOJjrfFbYzYtzky").roles("ADMIN");
+    }
+
 }
