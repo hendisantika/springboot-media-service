c9344afc6c02e770a6c5ae26ab298e2bbac0fbe7
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:36:57 +0700

    #6 - Add DocumentStoragePropertiesRepository

---
+++ src/main/java/com/hendisantika/springbootmediaservice/repository/DocumentStoragePropertiesRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootmediaservice.repository;
+
+import com.hendisantika.springbootmediaservice.entity.DocumentStorageProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-media-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/05/20
+ * Time: 09.36
+ */
+public interface DocumentStoragePropertiesRepository extends JpaRepository<DocumentStorageProperties, Integer> {
+
+}
