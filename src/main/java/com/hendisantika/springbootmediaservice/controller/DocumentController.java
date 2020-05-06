af755e0c42cbd640c40411345ba47d8f184ca635
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:56:24 +0700

    #19 - Add downloadFile API in DocumentController

--- src/main/java/com/hendisantika/springbootmediaservice/controller/DocumentController.java
+++ src/main/java/com/hendisantika/springbootmediaservice/controller/DocumentController.java
@@ -3,12 +3,20 @@ package com.hendisantika.springbootmediaservice.controller;
 import com.hendisantika.springbootmediaservice.dto.UploadFileResponse;
 import com.hendisantika.springbootmediaservice.service.DocumentStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,6 +44,38 @@ public class DocumentController {
                 .toUriString();
         return new UploadFileResponse(fileName, fileDownloadUri,
                 file.getContentType(), file.getSize());
+    }
+
+    @GetMapping("/downloadFile")
+    public ResponseEntity<Resource> downloadFile(@RequestParam("userId") Integer userId,
+                                                 @RequestParam("docType") String docType,
+                                                 HttpServletRequest request) {
+        String fileName = documentStorageService.getDocumentName(userId, docType);
+        Resource resource = null;
+        if (fileName != null && !fileName.isEmpty()) {
+            try {
+                resource = documentStorageService.loadFileAsResource(fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // Try to determine file's content type
+            String contentType = null;
+            try {
+                contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            } catch (IOException ex) {
+                //logger.info("Could not determine file type.");
+            }
+            // Fallback to the default content type if type could not be determined
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
