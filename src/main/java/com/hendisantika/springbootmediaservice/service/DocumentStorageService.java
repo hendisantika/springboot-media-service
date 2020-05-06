f442e62e8bd12609d847c5c6c497fe53f2df1eb7
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:45:43 +0700

    #11 - Add storeFile service in DocumentStorageService

--- src/main/java/com/hendisantika/springbootmediaservice/service/DocumentStorageService.java
+++ src/main/java/com/hendisantika/springbootmediaservice/service/DocumentStorageService.java
@@ -4,11 +4,14 @@ import com.hendisantika.springbootmediaservice.entity.DocumentStorageProperties;
 import com.hendisantika.springbootmediaservice.exception.DocumentStorageException;
 import com.hendisantika.springbootmediaservice.repository.DocumentStoragePropertiesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,5 +37,43 @@ public class DocumentStorageService {
                     "stored.", e);
         }
 
+    }
+
+    public String storeFile(MultipartFile file, Integer userId, String docType) {
+        // Normalize file name
+        String originalFileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String fileName = "";
+        try {
+            // Check if the file's name contains invalid characters
+            if (originalFileName.contains("..")) {
+                throw new DocumentStorageException("Sorry! Filename contains invalid path sequence " + originalFileName);
+            }
+            String fileExtension = "";
+            try {
+                fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+            } catch (Exception e) {
+                fileExtension = "";
+            }
+            fileName = userId + "_" + docType + fileExtension;
+            // Copy file to the target location (Replacing existing file with the same name)
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            DocumentStorageProperties doc = docStorageRepo.checkDocumentByUserId(userId, docType);
+            if (doc != null) {
+                doc.setDocumentFormat(file.getContentType());
+                doc.setFileName(fileName);
+                docStorageRepo.save(doc);
+            } else {
+                DocumentStorageProperties newDoc = new DocumentStorageProperties();
+                newDoc.setUserId(userId);
+                newDoc.setDocumentFormat(file.getContentType());
+                newDoc.setFileName(fileName);
+                newDoc.setDocumentType(docType);
+                docStorageRepo.save(newDoc);
+            }
+            return fileName;
+        } catch (IOException ex) {
+            throw new DocumentStorageException("Could not store file " + fileName + ". Please try again!", ex);
+        }
     }
 }
