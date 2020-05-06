968c25d3185f7c01dffbd91e04c816851b819808
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:38:24 +0700

    #8 - Add getUploadDocumentPath query method in DocumentStoragePropertiesRepository

--- src/main/java/com/hendisantika/springbootmediaservice/repository/DocumentStoragePropertiesRepository.java
+++ src/main/java/com/hendisantika/springbootmediaservice/repository/DocumentStoragePropertiesRepository.java
@@ -16,4 +16,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface DocumentStoragePropertiesRepository extends JpaRepository<DocumentStorageProperties, Integer> {
     @Query("Select a from DocumentStorageProperties a where user_id = ?1 and document_type = ?2")
     DocumentStorageProperties checkDocumentByUserId(Integer userId, String docType);
+
+    @Query("Select fileName from DocumentStorageProperties a where user_id = ?1 and document_type = ?2")
+    String getUploadDocumentPath(Integer userId, String docType);
 }
