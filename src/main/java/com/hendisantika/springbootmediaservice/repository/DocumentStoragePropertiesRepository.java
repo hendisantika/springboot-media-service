c7e228bae3a701f57b460fe4b8970f220256d40d
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:37:55 +0700

    #7 - Add checkDocumentByUserId query method in DocumentStoragePropertiesRepository

--- src/main/java/com/hendisantika/springbootmediaservice/repository/DocumentStoragePropertiesRepository.java
+++ src/main/java/com/hendisantika/springbootmediaservice/repository/DocumentStoragePropertiesRepository.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootmediaservice.repository;
 
 import com.hendisantika.springbootmediaservice.entity.DocumentStorageProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,5 +14,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Time: 09.36
  */
 public interface DocumentStoragePropertiesRepository extends JpaRepository<DocumentStorageProperties, Integer> {
-
+    @Query("Select a from DocumentStorageProperties a where user_id = ?1 and document_type = ?2")
+    DocumentStorageProperties checkDocumentByUserId(Integer userId, String docType);
 }
