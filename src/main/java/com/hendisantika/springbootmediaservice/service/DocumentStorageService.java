34e3aa5ca80e02a35f283be830a63c685c69365d
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:46:27 +0700

    #12 - Add loadFileAsResource service in DocumentStorageService

--- src/main/java/com/hendisantika/springbootmediaservice/service/DocumentStorageService.java
+++ src/main/java/com/hendisantika/springbootmediaservice/service/DocumentStorageService.java
@@ -4,10 +4,14 @@ import com.hendisantika.springbootmediaservice.entity.DocumentStorageProperties;
 import com.hendisantika.springbootmediaservice.exception.DocumentStorageException;
 import com.hendisantika.springbootmediaservice.repository.DocumentStoragePropertiesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -76,4 +80,20 @@ public class DocumentStorageService {
             throw new DocumentStorageException("Could not store file " + fileName + ". Please try again!", ex);
         }
     }
+
+    public Resource loadFileAsResource(String fileName) throws Exception {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new FileNotFoundException("File not found " + fileName);
+        }
+    }
+
+
 }
