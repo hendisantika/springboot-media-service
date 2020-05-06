c5bdb9b87143ca2199ec534be04406a2bbaa3d93
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 09:53:50 +0700

    #18 - Add uploadFile API in DocumentController

--- src/main/java/com/hendisantika/springbootmediaservice/controller/DocumentController.java
+++ src/main/java/com/hendisantika/springbootmediaservice/controller/DocumentController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootmediaservice.controller;
 
+import com.hendisantika.springbootmediaservice.dto.UploadFileResponse;
 import com.hendisantika.springbootmediaservice.service.DocumentStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class DocumentController {
     @Autowired
     private DocumentStorageService documentStorageService;
+
+    @PostMapping("uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file,
+                                         @RequestParam("userId") Integer UserId,
+                                         @RequestParam("docType") String docType) {
+        String fileName = documentStorageService.storeFile(file, UserId, docType);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+
 }
